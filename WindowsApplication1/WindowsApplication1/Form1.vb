0828da1d78134e01fd657e70d2e479de0c0291df
GaelAlricASCII <gael.alri@ascii.fr>
2018-05-17 11:50:04 +0200

    changement

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,5 +1,14 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        TextBox1.Text = "BONJOUR"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = "cc"
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim DialogResult = MessageBox.Show("Sure", "Some Title", MessageBoxButtons.YesNo)
+        If (DialogResult = DialogResult.No) Then
+            e.Cancel = True
+        End If
+
     End Sub
 End Class
